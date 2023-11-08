6f9f0462e42f105d9977636a7d8c3e91f008f8c0
Aradhya Nepal <aradhya.1221@gmail.com>
2023-11-08 15:16:22 +0545

    Solved the grep problem

--- analyse_script_to_test.sh
+++ analyse_script_to_test.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
 echo "Running flutter analyze..."
-echo "Running flutter analyze..."
-analysis_output=$(flutter analyze)
-echo "$analysis_output" > temp.txt
-total_warnings=$(grep -c '(error:|warning:|hint:|info:) (.*)' temp.txt)
-echo "Total Warnings: $total_warnings"
-rm temp.txt
-if [ "$total_warnings" -gt 10 ]; then
-  echo "More than 10 issues found. Terminating the workflow"
+flutter analyze > temp.txt
+total_lint=$(grep -c 'error - \|warning - \|hint - \|info - ' temp.txt)
+
+echo "Total Lint: $total_lint"
+
+
+
+if [ "$total_lint" -gt 10 ]; then
+  echo "More than 10 lint issues found. Terminating the workflow"
   exit 1
 fi
 
