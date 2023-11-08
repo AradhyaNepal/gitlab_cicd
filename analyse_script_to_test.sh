0299a0c406d8bda8fae1f93bf710ecbed421b381
Aradhya Nepal <aradhya.1221@gmail.com>
2023-11-08 14:37:57 +0545

    Test

--- analyse_script_to_test.sh
+++ analyse_script_to_test.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
 echo "Running flutter analyze..."
-analysis_output=$(flutter analyze)
-total_lines=$(echo "$analysis_output" | wc -l)
-# Two extra not our concern lines are printed on flutter analyze
-#Todo: This logic have error because on cicd there would be other messages too
-total_warning=$(($total_lines - 2))
-
-echo "Total Warnings: $total_warning"
-
-
-if [ "$total_warning" -gt 10 ]; then
-    echo "More than 10 issues found. Terminating the workflow"
-    exit 1
+flutter analyze >temp.txt
+total_warnings=$(grep -c 'warning:' temp.txt)
+echo "Total Warnings: $total_warnings"
+rm temp.txt
+if [ "$total_warnings" -gt 10 ]; then
+  echo "More than 10 issues found. Terminating the workflow"
+  exit 1
 fi
 
 exit 0
