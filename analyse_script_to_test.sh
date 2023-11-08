f991f58a2209ce6e2822891b8da8a038ff33ff0c
Aradhya Nepal <aradhya.1221@gmail.com>
2023-11-08 14:43:05 +0545

    Test 2 again

--- analyse_script_to_test.sh
+++ analyse_script_to_test.sh
@@ -2,7 +2,7 @@
 
 echo "Running flutter analyze..."
 flutter analyze >temp.txt
-total_warnings=$(grep -c 'warning:' temp.txt)
+total_warnings=$(grep -c 'warning: (.*)' temp.txt)
 echo "Total Warnings: $total_warnings"
 rm temp.txt
 if [ "$total_warnings" -gt 10 ]; then
